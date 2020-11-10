13cede7f0b5c73ff1a8015de7fdabc9ea89581e7
Prashant Ramnani <ramnani.prashant@gmail.com>
2020-11-10 12:12:16 +0530

    Start of second assignment

---
+++ Assignment-2/CodeSubmission/disk.c
@@ -0,0 +1,72 @@
+#include "disk.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int create_disk(disk *diskptr, int nbytes){
+	
+	if(diskptr==NULL)
+		return -1;
+
+	diskptr->size = 0;
+	diskptr->blocks=0;
+
+	int usable_size = nbytes - 24;
+	if(usable_size < BLOCKSIZE){
+		return -1;
+	}
+
+	diskptr->size = nbytes;
+	diskptr->blocks=usable_size/BLOCKSIZE;
+	diskptr->reads = diskptr->writes = 0;
+	diskptr->block_arr = (char **)malloc(diskptr->blocks*sizeof(char*));
+	for(int i=0;i<diskptr->blocks;i++)
+		diskptr->block_arr[i] = (char*)malloc(BLOCKSIZE*sizeof(char));
+
+	return 0;
+}
+
+int read_block(disk *diskptr, int blocknr, void *block_data){
+
+	if(diskptr==NULL)
+		return -1;
+	// Check if blocknr valid
+	int B = diskptr->blocks;
+	if(blocknr<0 || blocknr>=B){
+		return -1;
+	}
+
+	// Copy data from diskptr->block_arr[blocknr] to block_data
+	memcpy(block_data, diskptr->block_arr[blocknr], BLOCKSIZE);
+	diskptr->reads++;
+	return 0;
+}
+
+int write_block(disk *diskptr, int blocknr, void *block_data){
+
+	if(diskptr==NULL)
+		return -1;
+
+	// Check if blocknr valid
+	int B = diskptr->blocks;
+	if(blocknr<0 || blocknr>=B){
+		return -1;
+	}
+
+	// Copy data from diskptr->block_arr[blocknr] to block_data
+	memcpy(diskptr->block_arr[blocknr], block_data, BLOCKSIZE);
+	diskptr->writes++;
+	return 0;
+}
+
+int free_disk(disk *diskptr){
+
+	if(diskptr){
+		for(int i=0;i<diskptr->blocks;i++)
+			free(diskptr->block_arr[i]);
+		free(diskptr->block_arr);
+		free(diskptr);
+		return 0;
+	}
+	return -1;
+}
